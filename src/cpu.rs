3fa4dbfde4b6cda5d436448e8bb731ece834dd27
clawyq <clintonlaw96@gmail.com>
2024-08-29 00:04:26 +0800

    refactor common operations

--- src/cpu.rs
+++ src/cpu.rs
@@ -35,71 +35,52 @@ impl CPU {
                     return;
                 }
                 0xA2 => {
-                    // LDX
                     // Reads an extra byte for parameter
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
+                    self.ldx(param);
 
-                    // load into x
-                    self.register_x = param;
-
-                    // setting the Z(ero) flag
-                    if self.register_x == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-
-                    // Setting the N(egative) flag
-                    if self.register_x & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
                 }
                 0xA9 => {
-                    // LDA
                     // Reads an extra byte for parameter
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-
-                    // load into accumulator
-                    self.register_a = param;
-
-                    // setting the Z(ero) flag
-                    if self.register_a == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-
-                    // Setting the N(egative) flag
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
+                    self.lda(param);
                 }
-                0xAA => {
-                    //TAX
-                    self.register_x = self.register_a;
-
-                    // setting the Z(ero) flag
-                    if self.register_x == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-
-                    // Setting the N(egative) flag
-                    if self.register_x & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
-                }
+                0xAA => { self.tax(); }
                 _ => todo!(),
             }
+        }
+    }
+
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn ldx(&mut self, value: u8) {
+        self.register_x = value;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, value: u8) {
+        // setting the Z(ero) flag
+        if value == 0 {
+            self.status = self.status | 0b0000_0010;
+        } else {
+            self.status = self.status & 0b1111_1101;
+        }
+
+        // Setting the N(egative) flag
+        if value & 0b1000_0000 != 0 {
+            self.status = self.status | 0b1000_0000;
+        } else {
+            self.status = self.status & 0b0111_1111;
         }
     }
 }
