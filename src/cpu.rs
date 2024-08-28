66aa84995fa5326c3b12bc501ce228ac85cc9cb1
clawyq <clintonlaw96@gmail.com>
2024-08-28 23:58:50 +0800

    add TAX and tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -11,7 +11,7 @@ pub struct CPU {
  *  2. Decode instruction
  *  3. Execute instruction
  *  4. Repeat
- * 
+ *
  * Instruction reference according to https://www.nesdev.org/obelisk-6502-guide/reference.html
  */
 impl CPU {
@@ -31,15 +31,19 @@ impl CPU {
             let opscode = program[self.program_counter as usize];
             self.program_counter += 1;
             match opscode {
-                0x00 => { return; },
-                0xA2 => { // LDX
-                    // Reads an extra byte for parameter 
+                0x00 => {
+                    return;
+                }
+                0xA2 => {
+                    // LDX
+                    // Reads an extra byte for parameter
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
 
                     // load into x
                     self.register_x = param;
 
+                    // setting the Z(ero) flag
                     if self.register_x == 0 {
                         self.status = self.status | 0b0000_0010;
                     } else {
@@ -52,9 +56,10 @@ impl CPU {
                     } else {
                         self.status = self.status & 0b0111_1111;
                     }
-                },
-                0xA9 => { // LDA
-                    // Reads an extra byte for parameter 
+                }
+                0xA9 => {
+                    // LDA
+                    // Reads an extra byte for parameter
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
 
@@ -74,8 +79,26 @@ impl CPU {
                     } else {
                         self.status = self.status & 0b0111_1111;
                     }
-                },
-                _ => todo!()
+                }
+                0xAA => {
+                    //TAX
+                    self.register_x = self.register_a;
+
+                    // setting the Z(ero) flag
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    // Setting the N(egative) flag
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                _ => todo!(),
             }
         }
     }
@@ -83,36 +106,36 @@ impl CPU {
 
 #[cfg(test)]
 mod test {
-   use super::*;
- 
-   #[test]
-   fn test_0xa9_lda_immediate_load_data() {
-       let mut cpu = CPU::new();
-       cpu.interpret(vec![0xa9, 0x05, 0x00]);
-       assert_eq!(cpu.register_a, 0b0000_0101);
-       assert_eq!(cpu.status, 0);
-       assert!(cpu.status & 0b0000_0010 == 0b00); // Zero unset?
-       assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
-   }
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0b0000_0101);
+        assert_eq!(cpu.status, 0);
+        assert!(cpu.status & 0b0000_0010 == 0b00); // Zero unset?
+        assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
+    }
 
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
-       assert_eq!(cpu.register_a, 0x00);
-       assert_eq!(cpu.status, 2);
-       assert!(cpu.status & 0b0000_0010 == 0b10); // Zero set?
-       assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
+        assert_eq!(cpu.register_a, 0x00);
+        assert_eq!(cpu.status, 2);
+        assert!(cpu.status & 0b0000_0010 == 0b10); // Zero set?
+        assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
     }
 
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x09, 0x00]);
-       assert_eq!(cpu.register_a, 0x09);
-       assert_eq!(cpu.status, 0);
-       assert!(cpu.status & 0b0000_0010 == 0b00); // Zero set?
-       assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
+        assert_eq!(cpu.register_a, 0x09);
+        assert_eq!(cpu.status, 0);
+        assert!(cpu.status & 0b0000_0010 == 0b00); // Zero set?
+        assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
     }
 
     #[test]
@@ -124,24 +147,35 @@ mod test {
         assert!(cpu.status & 0b0000_0010 == 0b00); // Zero unset?
         assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
     }
- 
-     #[test]
-     fn test_0xa2_ldx_zero_flag() {
-         let mut cpu = CPU::new();
-         cpu.interpret(vec![0xa2, 0x00, 0x00]);
+
+    #[test]
+    fn test_0xa2_ldx_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa2, 0x00, 0x00]);
         assert_eq!(cpu.register_x, 0x00);
         assert_eq!(cpu.status, 2);
         assert!(cpu.status & 0b0000_0010 == 0b10); // Zero set?
         assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
-     }
- 
-     #[test]
-     fn test_0xa2_ldx_negative_flag() {
-         let mut cpu = CPU::new();
-         cpu.interpret(vec![0xa2, 0x09, 0x00]);
+    }
+
+    #[test]
+    fn test_0xa2_ldx_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa2, 0x09, 0x00]);
         assert_eq!(cpu.register_x, 0x09);
         assert_eq!(cpu.status, 0);
         assert!(cpu.status & 0b0000_0010 == 0b00); // Zero set?
         assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
-     }
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10);
+        assert!(cpu.status & 0b0000_0010 == 0b00); // Zero set?
+        assert!(cpu.status & 0b1000_0000 == 0); // Negative unset?
+    }
 }
