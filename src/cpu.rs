346d054c81340449aeb2a3dc9e3712dd17ec3f89
clawyq <clintonlaw96@gmail.com>
2024-08-28 23:19:02 +0800

    add LDA

--- src/cpu.rs
+++ src/cpu.rs
@@ -29,6 +29,28 @@ impl CPU {
             let opscode = program[self.program_counter as usize];
             self.program_counter += 1;
             match opscode {
+                0xA9 => { // LDA 
+                    // Reads an extra byte for parameter 
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+
+                    // load into the accumulator
+                    self.register_a = param;
+
+                    // setting the Z(ero) flag
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    // Setting the N(egative) flag
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                },
                 _ => todo!()
             }
         }
