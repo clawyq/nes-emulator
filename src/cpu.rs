28a87042a151da9d88c6e2413cb5e09fcd853ce5
clawyq <clintonlaw96@gmail.com>
2024-09-01 02:18:00 +0800

    init Mem trait

--- src/cpu.rs
+++ src/cpu.rs
@@ -39,7 +39,43 @@ pub struct CPU {
     pub status: StatusFlags,
     pub stack_ptr: u8,
     pub program_counter: u16,
-    memory: [u8; 0xFFFF],
+}
+
+pub trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
+
+    fn mem_write(&mut self, addr: u16, data: u8);
+
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+}
+
+impl Mem for CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.bus.mem_read(addr)
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.bus.mem_write(addr, data);
+    }
+
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        self.bus.mem_read_u16(pos)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        self.bus.mem_write_u16(pos, data)
+    }
 }
 
 /**
@@ -121,25 +157,6 @@ impl CPU {
                 panic!("Go to sleep. Why you tryna find a new address bruv.")
             }
         }
-    }
-
-    pub fn mem_read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
-    }
-
-    pub fn mem_write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
-    }
-
-    fn mem_read_u16(&self, addr: u16) -> u16 {
-        u16::from_le_bytes([self.mem_read(addr), self.mem_read(addr.wrapping_add(1))])
-    }
-
-    fn mem_write_u16(&mut self, addr: u16, data: u16) {
-        let lo = (data & 0xFF) as u8;
-        let hi = (data >> 8) as u8;
-        self.mem_write(addr, lo);
-        self.mem_write(addr + 1, hi);
     }
 
     fn push(&mut self, data: u8) {
